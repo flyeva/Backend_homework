547362eebbbbfbbcbc9f27fc3f7733ed39383cae
Eva Simetinger <eva.simetinger@gmail.com>
2020-11-09 09:22:07 +0100

    flask

---
+++ my_homepage.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+
+@app.route("projects/fakebook")
+def fakebook():
+    return render_template("index_fakebook.html")
+
+
+@app.route("/projects/stran22")
+def stran22():
+    return render_template("index_stran22.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
